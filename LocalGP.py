6512f4d4a4ab2171e1374d2051a3d43840a3ab0e
Hongjin Ren <renhj.aether@gmail.com>
2024-11-27 21:55:00 +0000

    update

--- LocalGP.py
+++ LocalGP.py
@@ -51,30 +51,31 @@ local_train_x, local_train_y = Tools.find_k_nearest_neighbors_CPU(input_point, t
 
 
 
-LocalGP_models, LocalGP_likelihoods = Training.train_one_row_LocalGP_Parallel(train_x, train_y, 
-                                                                                    test_y, row_idx, covar_type = 'RBF', k_num = 100,
-                                                                                    lr=0.025, num_iterations=5000, 
-                                                                                    patience=10, device=Device)
+LocalGP_models, LocalGP_likelihoods = Training.train_one_row_LocalGP_Parallel(train_x, train_y, test_y, row_idx, 
+                                                                              covar_type = 'RBF', k_num = 100,
+                                                                              lr=0.025, num_iterations=5000, 
+                                                                              patience=10, device=Device)
 
 
-full_test_preds = Prediction.full_preds(LocalGP_models_48, LocalGP_likelihoods_48, test_x[row_idx,:].unsqueeze(0).to(Device)).cpu().detach().numpy()
+full_test_preds = Prediction.full_preds(LocalGP_models, LocalGP_likelihoods, test_x[row_idx,:].unsqueeze(0).to(Device)).cpu().detach().numpy()
 
 
 
 bounds = bound.get_bounds(local_train_x)
 
-estimated_params, func_loss = Estimation.multi_start_estimation(LocalGP_models_48, LocalGP_likelihoods_48, row_idx, test_y_48, bounds, Estimation.estimate_params_Adam, 
+estimated_params, func_loss = Estimation.multi_start_estimation(LocalGP_models, LocalGP_likelihoods, row_idx, test_y, bounds, Estimation.estimate_params_Adam, 
                                                        num_starts=5, num_iterations=2000, lr=0.01, patience=50, attraction_threshold=0.1, repulsion_strength=0.1, device=Device)
 
 
 full_estimated_params = estimated_params.detach().numpy()
 
 
-mcmc_result_Normal_48 = Estimation.run_mcmc_Normal(Prediction.full_preds, LocalGP_models_48, LocalGP_likelihoods_48, row_idx, test_y_48, local_train_x, num_sampling=3000, warmup_step=1000)
+mcmc_result_Normal = Estimation.run_mcmc_Normal(Prediction.full_preds, LocalGP_models, LocalGP_likelihoods, row_idx, test_y, local_train_x, 
+                                                num_sampling=4000, warmup_step=1000, num_chains=2)
 
 
 
-posterior_samples_Normal = mcmc_result_Normal_48.get_samples()
+posterior_samples_Normal = mcmc_result_Normal.get_samples()
 param_names = [f'param_{i}' for i in range(len(bounds))]
 
 
@@ -90,41 +91,42 @@ for idx, param_name in enumerate(param_names):
 
 
 
-np.save('FixedG/LocalGP_full_preds_48.npy', full_test_preds)
+np.save('Result/LocalGP_full_preds.npy', full_test_preds)
 
-np.save('FixedG/LocalGP_full_estimated_params_48.npy', full_estimated_params)
+np.save('Result/LocalGP_full_estimated_params.npy', full_estimated_params)
 
-np.save('FixedG/LocalGP_posterior_means_48.npy', posterior_means_array)
-
-
-for row_idx in range(1,test_y_48.shape[0]):
-# for row_idx in range(1,2):
-    input_point = test_y_48[row_idx,:]
-
-    local_train_x, local_train_y = Tools.find_k_nearest_neighbors_CPU(input_point, train_x, train_y_48, k = 100)
+np.save('Result/LocalGP_posterior_means.npy', posterior_means_array)
 
 
-    LocalGP_models_48, LocalGP_likelihoods_48 = Training.train_one_row_LocalGP_Parallel(train_x, train_y_48, 
-                                                                              test_y_48, row_idx, covar_type = 'RQ', k_num = 100,
-                                                                              lr=0.025, num_iterations=5000, 
-                                                                              patience=10, device=Device)
+# for row_idx in range(1,test_y.shape[0]):
+for row_idx in range(1,2):
+    input_point = test_y[row_idx,:]
 
-    preds_tmp = Prediction.full_preds(LocalGP_models_48, LocalGP_likelihoods_48, test_x[row_idx,:].unsqueeze(0).to(Device)).cpu().detach().numpy()
+    local_train_x, local_train_y = Tools.find_k_nearest_neighbors_CPU(input_point, train_x, train_y, k = 100)
+
+
+    LocalGP_models, LocalGP_likelihoods = Training.train_one_row_LocalGP_Parallel(train_x, train_y, test_y, row_idx, 
+                                                                                  covar_type = 'RBF', k_num = 100,
+                                                                                  lr=0.025, num_iterations=5000, 
+                                                                                  patience=10, device=Device)
+
+    preds_tmp = Prediction.full_preds(LocalGP_models, LocalGP_likelihoods, test_x[row_idx,:].unsqueeze(0).to(Device)).cpu().detach().numpy()
     full_test_preds = np.vstack((full_test_preds, preds_tmp))
-    np.save('FixedG/LocalGP_full_preds_48.npy', full_test_preds)
+    np.save('FixedG/LocalGP_full_preds.npy', full_test_preds)
 
     bounds = bound.get_bounds(local_train_x)
 
-    estimated_params_tmp, estimated_params_loss_tmp = Estimation.multi_start_estimation(LocalGP_models_48, LocalGP_likelihoods_48, row_idx, test_y_48, bounds, Estimation.estimate_params_Adam, 
+    estimated_params_tmp, estimated_params_loss_tmp = Estimation.multi_start_estimation(LocalGP_models, LocalGP_likelihoods, row_idx, test_y, bounds, Estimation.estimate_params_Adam, 
                                                        num_starts=5, num_iterations=2000, lr=0.01, patience=50, attraction_threshold=0.1, repulsion_strength=0.1, device=Device)
 
     full_estimated_params = np.vstack((full_estimated_params, estimated_params_tmp.detach().numpy()))
-    np.save('FixedG/LocalGP_full_estimated_params_48.npy', full_estimated_params)
+    np.save('FixedG/LocalGP_full_estimated_params.npy', full_estimated_params)
 
 
-    mcmc_result_Normal_48 = Estimation.run_mcmc_Normal(Prediction.full_preds, LocalGP_models_48, LocalGP_likelihoods_48, row_idx, test_y_48, local_train_x, num_sampling=3000, warmup_step=1000)
+    mcmc_result_Normal = Estimation.run_mcmc_Normal(Prediction.full_preds, LocalGP_models, LocalGP_likelihoods, row_idx, test_y, local_train_x, 
+                                                    num_sampling=4000, warmup_step=1000, num_chains=2)
 
-    posterior_samples_Normal = mcmc_result_Normal_48.get_samples()
+    posterior_samples_Normal = mcmc_result_Normal.get_samples()
 
     posterior_means_array_tmp = np.zeros(len(param_names))
 
@@ -137,9 +139,9 @@ for row_idx in range(1,test_y_48.shape[0]):
         posterior_means_array_tmp[idx] = mean_value
 
     posterior_means_array = np.vstack((posterior_means_array, posterior_means_array_tmp))
-    np.save('FixedG/LocalGP_posterior_means_48.npy', posterior_means_array)
+    np.save('FixedG/LocalGP_posterior_means.npy', posterior_means_array)
     
 
 
 
-# nohup python MGP_10_48.py > MulGPout.log 2>&1 &
+# nohup python LocalGP.py > LocalGPout.log 2>&1 &
