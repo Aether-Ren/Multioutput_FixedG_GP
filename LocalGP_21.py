d61137fcd3b1c7f75e433c8694640b8b2238855b
Hongjin Ren <renhj.aether@gmail.com>
2025-01-09 11:01:28 +0000

    update LocelGP

--- LocalGP_21.py
+++ LocalGP_21.py
@@ -3,14 +3,11 @@ import gpytorch
 import pandas as pd
 import numpy as np
 import tqdm as tqdm
-import pyro
-import math
-import pickle
-import time
+
 from sklearn.preprocessing import StandardScaler
 from joblib import Parallel, delayed
 from sklearn.decomposition import PCA
-import matplotlib.pyplot as plt
+import os
 
 import pyro.distributions as dist
 from pyro.infer import MCMC, NUTS
@@ -55,107 +52,58 @@ pca_20.fit(train_y[:,1:])
 
 Device = 'cpu'
 
-row_idx = 0
 
-input_point = test_y_21[row_idx,:]
-local_train_x, local_train_y = Tools.find_k_nearest_neighbors_CPU(input_point, train_x, train_y_21, k = 100)
+output_file = 'Result/LocalGP_21_result.csv'
 
 
+if not os.path.exists(output_file):
+    with open(output_file, 'w') as f:
+        f.write('Iteration,test_preds,estimated_params,posterior_means\n')
 
-LocalGP_models, LocalGP_likelihoods = Training.train_one_row_LocalGP_Parallel(train_x, train_y_21, test_y_21, row_idx, 
-                                                                              covar_type = 'RBF', k_num = 100,
-                                                                              lr=0.025, num_iterations=5000, 
-                                                                              patience=10, device=Device)
+for row_idx in range(test_y_21.shape[0]):
+    input_point = test_y_21[row_idx, :]
 
+    local_train_x, local_train_y = Tools.find_k_nearest_neighbors_CPU(input_point, train_x, train_y_21, k=100)
 
-full_test_preds = Prediction.full_preds(LocalGP_models, LocalGP_likelihoods, test_x[row_idx,:].unsqueeze(0).to(Device)).cpu().detach().numpy()
-
-
-
-bounds = bound.get_bounds(local_train_x)
-
-estimated_params, func_loss = Estimation.multi_start_estimation(LocalGP_models, LocalGP_likelihoods, row_idx, test_y_21, bounds, Estimation.estimate_params_Adam, 
-                                                                num_starts=5, num_iterations=2000, lr=0.01, patience=50, 
-                                                                attraction_threshold=0.1, repulsion_strength=0.1, device=Device)
+    LocalGP_models, LocalGP_likelihoods = Training.train_one_row_LocalGP_Parallel(
+        train_x, train_y_21, test_y_21, row_idx,
+        covar_type='RBF', k_num=100, lr=0.025,
+        num_iterations=5000, patience=10, device=Device
+    )
 
 
-full_estimated_params = estimated_params.detach().numpy()
+    preds_tmp = Prediction.full_preds(
+        LocalGP_models, LocalGP_likelihoods, test_x[row_idx, :].unsqueeze(0).to(Device)
+    ).cpu().detach().numpy()
 
-
-mcmc_result_Uniform = Estimation.run_mcmc_Uniform(Prediction.full_preds, LocalGP_models, LocalGP_likelihoods, row_idx, test_y, local_train_x, 
-                                                  PCA_func = pca_20,
-                                                  num_sampling=4000, warmup_step=1000, num_chains=1)
-
-
-
-posterior_samples_Uniform = mcmc_result_Uniform.get_samples()
-param_names = [f'param_{i}' for i in range(len(bounds))]
-
-
-posterior_means_array = np.zeros(len(param_names))
-
-
-for idx, param_name in enumerate(param_names):
-    samples = posterior_samples_Uniform[param_name]
-    if samples.ndim > 1:
-        samples = samples.reshape(-1)
-    mean_value = torch.mean(samples).item()
-    posterior_means_array[idx] = mean_value
-
-
-
-np.save('Result/LocalGP_full_preds_21.npy', full_test_preds)
-
-np.save('Result/LocalGP_full_estimated_params_21.npy', full_estimated_params)
-
-np.save('Result/LocalGP_posterior_means_21.npy', posterior_means_array)
-
-
-for row_idx in range(1,test_y_21.shape[0]):
-# for row_idx in range(1,2):
-    input_point = test_y_21[row_idx,:]
-
-    local_train_x, local_train_y = Tools.find_k_nearest_neighbors_CPU(input_point, train_x, train_y_21, k = 100)
-
-
-    LocalGP_models, LocalGP_likelihoods = Training.train_one_row_LocalGP_Parallel(train_x, train_y_21, test_y_21, row_idx, 
-                                                                                  covar_type = 'RBF', k_num = 100,
-                                                                                  lr=0.025, num_iterations=5000, 
-                                                                                  patience=10, device=Device)
-
-    preds_tmp = Prediction.full_preds(LocalGP_models, LocalGP_likelihoods, test_x[row_idx,:].unsqueeze(0).to(Device)).cpu().detach().numpy()
-    full_test_preds = np.vstack((full_test_preds, preds_tmp))
-    np.save('Result/LocalGP_full_preds_21.npy', full_test_preds)
 
     bounds = bound.get_bounds(local_train_x)
-
-    estimated_params_tmp, estimated_params_loss_tmp = Estimation.multi_start_estimation(LocalGP_models, LocalGP_likelihoods, row_idx, test_y_21, bounds, Estimation.estimate_params_Adam, 
-                                                                                        num_starts=5, num_iterations=2000, lr=0.01, patience=50, 
-                                                                                        attraction_threshold=0.1, repulsion_strength=0.1, device=Device)
-
-    full_estimated_params = np.vstack((full_estimated_params, estimated_params_tmp.detach().numpy()))
-    np.save('Result/LocalGP_full_estimated_params_21.npy', full_estimated_params)
+    estimated_params_tmp, _ = Estimation.multi_start_estimation(
+        LocalGP_models, LocalGP_likelihoods, row_idx, test_y_21, bounds,
+        Estimation.estimate_params_Adam, num_starts=5, num_iterations=2000, lr=0.01,
+        patience=50, attraction_threshold=0.1, repulsion_strength=0.1, device=Device
+    )
 
 
-    mcmc_result_Uniform = Estimation.run_mcmc_Uniform(Prediction.full_preds, LocalGP_models, LocalGP_likelihoods, row_idx, test_y, local_train_x, 
-                                                      PCA_func = pca_20, 
-                                                      num_sampling=4000, warmup_step=1000, num_chains=1)
-
+    mcmc_result_Uniform = Estimation.run_mcmc_Uniform(
+        Prediction.full_preds, LocalGP_models, LocalGP_likelihoods, row_idx, test_y,
+        local_train_x, PCA_func=pca_20, num_sampling=4000, warmup_step=1000, num_chains=1
+    )
     posterior_samples_Uniform = mcmc_result_Uniform.get_samples()
 
+    param_names = [f'param_{i}' for i in range(len(bounds))]
     posterior_means_array_tmp = np.zeros(len(param_names))
-
 
     for idx, param_name in enumerate(param_names):
         samples = posterior_samples_Uniform[param_name]
         if samples.ndim > 1:
             samples = samples.reshape(-1)
-        mean_value = torch.mean(samples).item()
-        posterior_means_array_tmp[idx] = mean_value
+        posterior_means_array_tmp[idx] = torch.mean(samples).item()
 
-    posterior_means_array = np.vstack((posterior_means_array, posterior_means_array_tmp))
-    np.save('Result/LocalGP_posterior_means_21.npy', posterior_means_array)
-    
+
+    with open(output_file, 'a') as f:
+        f.write(f"{row_idx + 1},\"{list(preds_tmp)}\",\"{list(estimated_params_tmp.detach().numpy())}\",\"{list(posterior_means_array_tmp)}\"\n")
+
 
 
 
