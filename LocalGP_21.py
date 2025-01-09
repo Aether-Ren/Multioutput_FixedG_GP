b2c48271d9b84c40e985873b3379416ef7d846f4
Hongjin Ren <renhj.aether@gmail.com>
2025-01-09 11:02:36 +0000

    0

--- LocalGP_21.py
+++ LocalGP_21.py
@@ -86,8 +86,10 @@ for row_idx in range(test_y_21.shape[0]):
 
 
     mcmc_result_Uniform = Estimation.run_mcmc_Uniform(
-        Prediction.full_preds, LocalGP_models, LocalGP_likelihoods, row_idx, test_y,
-        local_train_x, PCA_func=pca_20, num_sampling=4000, warmup_step=1000, num_chains=1
+        Prediction.full_preds, LocalGP_models, LocalGP_likelihoods, 
+        row_idx, test_y, local_train_x, 
+        PCA_func=pca_20, 
+        num_sampling=4000, warmup_step=1000, num_chains=1
     )
     posterior_samples_Uniform = mcmc_result_Uniform.get_samples()
 
