53c619d37fbd046f851eac5099fa02c65641c4d0
Aether-Ren <renhj.aether@gmail.com>
2025-11-23 22:22:12 +0000

    update

---
+++ MVGP_LocalD_MCMC.py
@@ -0,0 +1,88 @@
+import torch
+import gpytorch
+import pandas as pd
+import numpy as np
+import tqdm as tqdm
+
+
+import os
+import pickle
+
+import GP_functions.Loss_function as Loss_function
+import GP_functions.bound as bound
+import GP_functions.Estimation as Estimation
+import GP_functions.Training as Training
+import GP_functions.Prediction as Prediction
+import GP_functions.GP_models as GP_models
+import GP_functions.Tools as Tools
+import GP_functions.FeatureE as FeatureE
+
+X_train = pd.read_csv('Data/X_train.csv', header=None, delimiter=',').values
+X_test = pd.read_csv('LocalDisease/X_2.csv', header=None, delimiter=',').values
+
+Y_train_pca = pd.read_csv('Data/Y_train_std_21.csv', header=None, delimiter=',').values
+Y_test_21 = pd.read_csv('LocalDisease/Y_data_2_pca.csv', header=None, delimiter=',').values
+
+# X_edge = pd.read_csv('Data/X_edge.csv', header=None, delimiter=',').values
+# Y_edge_std_pca = pd.read_csv('LocalDisease/Y_edge_std_pca.csv', header=None, delimiter=',').values
+
+# X_train = np.vstack([X_train, X_edge])
+# Y_train_pca = np.vstack([Y_train_pca, Y_edge_std_pca])
+
+
+train_x = torch.tensor(X_train, dtype=torch.float32)
+test_x = torch.tensor(X_test, dtype=torch.float32)
+
+train_y_21 = torch.tensor(Y_train_pca, dtype=torch.float32)
+test_y_21 = torch.tensor(Y_test_21, dtype=torch.float32)
+
+
+
+torch.set_default_dtype(torch.float32)
+
+####################################################################
+
+Device = 'cuda'
+
+
+mcmc_dir = 'LocalDisease/Result/MVGP_21_mcmc_result'
+if not os.path.exists(mcmc_dir):
+    os.makedirs(mcmc_dir)
+
+checkpoint = torch.load('multitask_gp_checkpoint_21.pth', map_location=Device)
+model_params = checkpoint['model_params']
+
+MVGP_models = GP_models.MultitaskVariationalGP(train_x, train_y_21, 
+                                               num_latents=model_params['num_latents'], 
+                                               num_inducing=model_params['num_inducing'], 
+                                               covar_type=model_params['covar_type']).to(Device)
+
+MVGP_models.load_state_dict(checkpoint['model_state_dict'])
+
+MVGP_likelihoods = gpytorch.likelihoods.MultitaskGaussianLikelihood(num_tasks=train_y_21.shape[1]).to(Device)
+MVGP_likelihoods.load_state_dict(checkpoint['likelihood_state_dict'])
+
+MVGP_models.eval()
+MVGP_likelihoods.eval()
+
+
+for row_idx in range(test_y_21.shape[0]):
+    input_point = test_y_21[row_idx, :]
+
+    # local_train_x, local_train_y = Tools.find_k_nearest_neighbors_CPU(input_point, train_x, train_y_21, k=100)
+    local_train_x, local_train_y = Tools.find_k_nearest_neighbors_GPU(input_point, train_x, train_y_21, k=100)
+
+    bounds = bound.get_bounds(local_train_x)
+    mcmc_result_Uniform = Estimation.run_mcmc_Uniform(
+        Prediction.preds_distribution, MVGP_models, MVGP_likelihoods, 
+        row_idx, test_y_21, bounds, 
+        num_sampling=1200, warmup_step=300, num_chains=1, device=Device
+    )
+    posterior_samples_Uniform = mcmc_result_Uniform.get_samples()
+
+
+    mcmc_file = os.path.join(mcmc_dir, f'result_{row_idx + 1}.pkl')
+    with open(mcmc_file, 'wb') as f:
+        pickle.dump(posterior_samples_Uniform, f)
+
+# nohup python MVGP_LocalD_MCMC.py > MVGP_LocalD_MCMCout.log 2>&1 &
